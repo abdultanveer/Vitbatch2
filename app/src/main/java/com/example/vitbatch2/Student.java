a43ad235d860b9c31bc8ae5489293a3bd50af221
abdul <abdul.tanveer@gmail.com>
2024-06-07 19:15:53 +0530

    pojo classes in java n kotlin

---
+++ app/src/main/java/com/example/vitbatch2/Student.java
@@ -0,0 +1,43 @@
+package com.example.vitbatch2;
+
+/**
+ * POJO = plain old java object, VO = value object
+ */
+public class Student {
+    String name;
+    int age;
+    String address;
+
+    //ctrl+shift+A -- shortcut for all shortcuts
+
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ app/src/main/java/com/example/vitbatch2/kotlinegs/MyDemo.kt
@@ -0,0 +1,5 @@
+package com.example.vitbatch2.kotlinegs
+
+fun main() {
+    println("hello world")
+}
