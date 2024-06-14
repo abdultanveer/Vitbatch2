f8e32e61956d278e9072d61ed35e65fdd0891c09
abdul <abdul.tanveer@gmail.com>
2024-06-14 19:11:48 +0530

    recyclerview - part2 adapter file

---
+++ app/src/main/java/com/example/vitbatch2/DataAdaper.kt
@@ -0,0 +1,9 @@
+package com.example.vitbatch2
+
+import androidx.recyclerview.widget.RecyclerView
+
+class DataAdaper: RecyclerView.Adapter<DataViewHolder>() {
+    class DataViewHolder {
+
+    }
+}
