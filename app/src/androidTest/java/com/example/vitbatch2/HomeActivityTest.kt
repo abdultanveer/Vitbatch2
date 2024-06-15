0c13c93f8af46274dbc2289a6d56eef620fecd74
abdul <abdul.tanveer@gmail.com>
2024-06-15 19:29:54 +0530

    recorded espresso ui test

---
+++ app/src/androidTest/java/com/example/vitbatch2/HomeActivityTest.kt
@@ -0,0 +1,176 @@
+package com.example.vitbatch2
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class HomeActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Test
+    fun homeActivityTest() {
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.etUi),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("Android workshops"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.etUi), withText("Android workshops"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(click())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.etUi), withText("Android workshops"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(click())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.etUi), withText("Android workshops"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(replaceText("Android workshops@Gmail. com"))
+
+        val appCompatEditText5 = onView(
+            allOf(
+                withId(R.id.etUi), withText("Android workshops@Gmail. com"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText5.perform(closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btnUi), withText("get text"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.main),
+                        childAtPosition(
+                            withId(android.R.id.content),
+                            0
+                        )
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.tvUi), withText("Android workshops@Gmail. com"),
+                withParent(
+                    allOf(
+                        withId(R.id.main),
+                        withParent(withId(android.R.id.content))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Android workshops@Gmail. com")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/vitbatch2/HomeActivity.kt
+++ app/src/main/java/com/example/vitbatch2/HomeActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,11 +17,13 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     var TAG = HomeActivity::class.java.simpleName   //"HomeActivity"
 
     var data = arrayOf("india","english","android","computers")
+    var b = 20
 
     lateinit var mySpinner: Spinner
     lateinit var recyclerview:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var a = 10
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         mySpinner = findViewById(R.id.spinner)
@@ -45,10 +49,21 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
        var item = adapter?.selectedItem.toString()
         Log.i(TAG,item)
+        println(b)
+
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
         TODO("Not yet implemented")
+    }
+
+    fun getShowText(view: View) {
+        //get the text from et
+        var uiEt:EditText = findViewById(R.id.etUi)
+        var textTyped = uiEt.text.toString()
+        //show the text in the tv
+        var uiTv: TextView = findViewById(R.id.tvUi)
+        uiTv.setText(textTyped)
     }
 
 }
