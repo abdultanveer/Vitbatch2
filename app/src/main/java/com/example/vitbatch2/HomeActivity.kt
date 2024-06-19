23e934bf76ea35f907153782f99bb9ac320f6b74
abdul <abdul.tanveer@gmail.com>
2024-06-19 19:18:04 +0530

    setting the parsed json into reyclerview

--- app/src/main/java/com/example/vitbatch2/HomeActivity.kt
+++ app/src/main/java/com/example/vitbatch2/HomeActivity.kt
@@ -8,16 +8,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vitbatch2.network.MarsAdapter
 import com.example.vitbatch2.network.MarsApi
+import com.example.vitbatch2.network.MarsPhoto
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
      //lateinit var brake:Int?
+    lateinit var recyclerview:RecyclerView
+    lateinit var listMarsPhotos:List<MarsPhoto>
+    lateinit var marsAdapter: MarsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        recyclerview = findViewById(R.id.recyclerView)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        listMarsPhotos = ArrayList<MarsPhoto>()
+        marsAdapter = MarsAdapter(listMarsPhotos)
+        recyclerview.adapter = marsAdapter
        // brake
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -32,17 +45,17 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
+
 
     fun getJson(view: View) {
         getMarsPhotos()
     }
 
     private fun getMarsPhotos() {
-        GlobalScope.launch {
+        GlobalScope.async {
             val listMarsPhoto = MarsApi.retrofitService.getPhotos()
+            listMarsPhotos = listMarsPhoto
+            marsAdapter.notifyItemRangeChanged(0,listMarsPhotos.size)
             Log.i("HomeActivity-1st imgsrc",listMarsPhoto.get(0).imgSrc)
 
         }
