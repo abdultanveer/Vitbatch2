1d9e18e9af3edae53674500dda03db2ace4d70b7
abdul <abdul.tanveer@gmail.com>
2024-06-20 19:41:59 +0530

    viewbinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
 android {
     namespace = "com.example.vitbatch2"
     compileSdk = 34
+    buildFeatures{
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.vitbatch2"
--- app/src/main/java/com/example/vitbatch2/HomeActivity.kt
+++ app/src/main/java/com/example/vitbatch2/HomeActivity.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.vitbatch2.databinding.ActivityHomeBinding
 import com.example.vitbatch2.network.MarsAdapter
 import com.example.vitbatch2.network.MarsApi
 import com.example.vitbatch2.network.MarsPhoto
@@ -22,21 +23,28 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
-     //lateinit var brake:Int?
-    lateinit var recyclerview:RecyclerView
+
+    private lateinit var binding: ActivityHomeBinding
+
+    //lateinit var brake:Int?
+   // lateinit var recyclerview:RecyclerView
     lateinit var listMarsPhotos:List<MarsPhoto>
     lateinit var marsAdapter: MarsAdapter
    // lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        //setContentView(R.layout.activity_home)
+       binding = ActivityHomeBinding.inflate(layoutInflater)
+       val view = binding.root
+       setContentView(view)
+
        // imageView = findViewById(R.id.imageView)
-        recyclerview = findViewById(R.id.recyclerView)
-        recyclerview.layoutManager = LinearLayoutManager(this)
+       // recyclerview = findViewById(R.id.recyclerView)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
         listMarsPhotos = ArrayList<MarsPhoto>()
         marsAdapter = MarsAdapter(listMarsPhotos)
-        recyclerview.adapter = marsAdapter
+        binding.recyclerView.adapter = marsAdapter
        // brake
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -46,16 +54,24 @@ class HomeActivity : AppCompatActivity() {
       //  if(intent.extras != null) {
             var data = intent.extras?.getString("nkey")
         Log.i("HomeActivity","data is = "+data)
-         val homeTextView:TextView = findViewById(R.id.tvHome)
-         homeTextView.setText(data)
+        // val homeTextView:TextView = findViewById(R.id.tvHome)
+       binding.tvHome.setText(data)
+        // homeTextView.setText(data)
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.btnGet.setOnClickListener{
+            getMarsPhotos()
+        }
     }
 
 
 
-    fun getJson(view: View) {
+   /* fun getJson(view: View) {
         getMarsPhotos()
-    }
+    }*/
 
     private fun getMarsPhotos() {
         GlobalScope.launch (Dispatchers.Main){
@@ -63,6 +79,7 @@ class HomeActivity : AppCompatActivity() {
 
             val listMarsPhoto = MarsApi.retrofitService.getPhotos()
             marsAdapter.listMarsPhotos = listMarsPhoto
+            binding.imageView.load(listMarsPhoto.get(0).imgSrc)
            // imageView.load(listMarsPhoto.get(0).imgSrc)
             marsAdapter.notifyItemRangeChanged(0,listMarsPhoto.size)
           //  listMarsPhotos  = listMarsPhoto
