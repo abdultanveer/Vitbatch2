2f7d87e498cdbe25f112a7ddc37ed59c134415c8
abdul <abdul.tanveer@gmail.com>
2024-07-06 18:33:00 +0530

    sms broadcastreceiver

---
+++ app/src/main/java/com/example/vitbatch2/SmsReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.vitbatch2
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.util.Log
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val message = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        val content =  message[0].displayMessageBody
+        val phno = message[0].displayOriginatingAddress
+       Log.i("SmsReceiver","hi abdul you've an sms"+ content+phno)
+    }
+}
